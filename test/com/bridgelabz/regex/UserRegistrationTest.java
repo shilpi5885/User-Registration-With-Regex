d62e0de97c0553c0939382913c34c5e7c81cfb9d
shilpi5885 <94815031+shilpi5885@users.noreply.github.com>
2022-02-16 23:07:48 +0530

    Update UserRegistrationTest.java

--- test/com/bridgelabz/regex/UserRegistrationTest.java
+++ test/com/bridgelabz/regex/UserRegistrationTest.java
@@ -3,39 +3,33 @@ package com.bridgelabz.regex;
 import junit.framework.TestCase;
 
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 public class UserRegistrationTest extends TestCase {
-	private UserRegistration user;
-
-	@Before
-	public void UserRegistrationObj() {
-		user = new UserRegistration();
-	}
+	private UserRegistration user = new UserRegistration();;
 
 	@Test
 	public void testFirstName_thenAssertionSucceeds() {
-		assertTrue(user.isValidateFirstName("Kalpesh"));
+		assertTrue(user.isValidateFirstName("Shilpi"));
 		System.out.println("Valid First Name");
 	}
 
 	@Test
 	public void testFirstName_thenAssertionFail() {
-		assertTrue(user.isValidateFirstName("bunny"));
-		System.out.println("Valid First Name");
+		assertFalse(user.isValidateFirstName("bunny"));
+		System.out.println("Invalid First Name");
 	}
 
 	@Test
 	public void testLastName_thenAssertionSucceeds() {
-		assertTrue(user.isValidateLastName("Mahajan"));
+		assertTrue(user.isValidateLastName("Lamba"));
 		System.out.println("Valid Last Name");
 	}
 
 	@Test
 	public void testLastName_thenAssertionFail() {
-		assertTrue(user.isValidateLastName("devil"));
-		System.out.println("Valid Last Name");
+		assertFalse(user.isValidateLastName("devil"));
+		System.out.println("Invalid Last Name");
 	}
 
 	@Test
@@ -46,19 +40,19 @@ public class UserRegistrationTest extends TestCase {
 
 	@Test
 	public void testEmail_thenAssertionFail() {
-		assertTrue(user.isValidateEmail("abc@gmail@.com@"));
-		System.out.println("Valid Email");
+		assertFalse(user.isValidateEmail("abc@gmail@.com@"));
+		System.out.println("Invalid Email");
+	}
+
+	@Test
+	public void testMobileNumber_thenAssertionFails() {
+		assertFalse(user.isValidateMobileNumber("7738511504"));
+		System.out.println("Invalid Mobile Number");
 	}
 
 	@Test
 	public void testMobileNumber_thenAssertionSucceeds() {
-		assertTrue(user.isValidateMobileNumber("9876543210"));
-		System.out.println("Valid Mobile Number");
-	}
-
-	@Test
-	public void testMobileNumber_thenAssertionFail() {
-		assertTrue(user.isValidateMobileNumber("91 0876543210"));
+		assertTrue(user.isValidateMobileNumber("91 7738511504"));
 		System.out.println("Valid Mobile Number");
 	}
 
@@ -69,32 +63,27 @@ public class UserRegistrationTest extends TestCase {
 	}
 
 	@Test
+	public void testPassword_thenAssertionFails() {
+		assertFalse(user.isValidatePassword("Abcdefg@"));
+		System.out.println("Invalid Password");
+	}
+
+	@Test
+	public void testPassword_thenAssertionFails1() {
+		assertFalse(user.isValidatePassword("1221212234"));
+		System.out.println("Invalid Password");
+	}
+
+	@Test
 	public void testPassword_thenAssertionSucceeds1() {
-		assertTrue(user.isValidatePassword("Abcdefg@"));
-		System.out.println("Valid Password");
-	}
-
-	@Test
-	public void testPassword_thenAssertionSucceeds2() {
-		assertTrue(user.isValidatePassword("1221212234"));
-		System.out.println("Valid Password");
-	}
-
-	@Test
-	public void testPassword_thenAssertionSucceeds3() {
 		assertTrue(user.isValidatePassword("Ab@1234@@"));
 		System.out.println("Valid Password");
 	}
 
 	@Test
 	public void testPassword_thenAssertionFail() {
-		assertTrue(user.isValidatePassword("bbb"));
-		System.out.println("Valid Password");
-	}
-
-	@Test
-	public void whenUserRegistrationExceptionThrown_thenAssertionSucceeds() {
-		assertTrue(user.isValidatePassword("bbb"));
+		assertFalse(user.isValidatePassword("bbb"));
+		System.out.println("Invalid Password");
 	}
 
 	@After
